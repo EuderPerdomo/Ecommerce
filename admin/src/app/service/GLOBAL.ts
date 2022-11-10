84c9b84c1b389f945253bd948dc16c3e463d63c9
Daciros <daciros3284@gmail.com>
2022-11-10 09:30:15 -0500

    configuradtion appservices ci workflows

--- admin/src/app/service/GLOBAL.ts
+++ admin/src/app/service/GLOBAL.ts
@@ -1,5 +1,6 @@
 export var GLOBAL = {
-    url : 'http://127.0.0.1:4201/api/',
+    /*url : 'http://127.0.0.1:4201/api/',*/
     /* url : 'https://api.linajeperuano.com/api/', */
+    url : 'https://tienda-el-silicio.web.app/api/'
 
 }
--- tienda/src/app/services/GLOBA.ts
+++ tienda/src/app/services/GLOBA.ts
@@ -1,4 +1,7 @@
 export var GLOBAL = {
-    url: 'http://127.0.0.1:4201/api/',
-    socket: 'http://localhost:4201'
+    /*url: 'http://127.0.0.1:4201/api/',
+    socket: 'http://localhost:4201'*/
+
+    url: 'https://tienda-el-silicio.web.app/api',
+    socket: 'https://tienda-el-silicio.web.app'
 }
